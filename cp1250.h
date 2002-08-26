33ef771b1b92e652b12cef4231e83c9a40acdd04
cvs2git <cvs2git>
2002-08-26 22:17:15 +0000

    This commit was manufactured by cvs2svn to create tag 'jpilot'.

--- cp1250.h
+++
@@ -1,9 +0,0 @@
-/*
-	Header for Czech library
-	Convert Palm  <-> Unix:
-		Palm : CP 1250
-		Unix : ISO-8859-2
-*/
-
-void Win2Lat(unsigned char *const buf, int buf_len);
-void Lat2Win(unsigned char *const buf, int buf_len);
--- russian.h
+++
@@ -1,9 +0,0 @@
-/*
-	Header for Russian library
-	Convert Palm  <-> Unix:
-		Palm : koi8
-		Unix : Win1251
-*/
-
-void koi8_to_win1251(unsigned char *const buf, int buf_len);
-void win1251_to_koi8(unsigned char *const buf, int buf_len);
