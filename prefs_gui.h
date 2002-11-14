b92e79a05d9788e3a0b07dfbe5312d3ba2972cf1
Judd Montgomery <judd@jpilot.org>
2002-11-14 02:48:21 +0000

    updated copyright

--- prefs_gui.h
+++ prefs_gui.h
@@ -1,7 +1,7 @@
 /* prefs_gui.h
  * A module of J-Pilot http://jpilot.org
  * 
- * Copyright (C) 1999-2001 by Judd Montgomery
+ * Copyright (C) 1999-2002 by Judd Montgomery
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
