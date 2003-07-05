7105ef800d95d94c63978a14db9076a1d5a01b4d
Judd Montgomery <judd@jpilot.org>
2003-07-05 18:28:40 +0000

    experimental font code

--- jpilot.c
+++ jpilot.c
@@ -1513,7 +1513,27 @@ static gint cb_output2(GtkWidget *widget, GdkEventButton *event, gpointer data)
    return 0;
 }
 
+/* #define FONT_TEST */
 #ifdef FONT_TEST
+
+# ifdef ENABLE_GTK2
+void SetFontRecursively2(GtkWidget *widget, gpointer data)
+{
+   GtkStyle *style;
+   char *font_desc;
+
+   font_desc = (char *)data;
+
+   g_print("font desc=[%s]\n", font_desc);
+   style = gtk_widget_get_style(widget);
+   pango_font_description_free(style->font_desc);
+   style->font_desc = pango_font_description_from_string(font_desc);
+   gtk_widget_set_style(widget, style);
+   if (GTK_IS_CONTAINER(widget)) {
+      gtk_container_foreach(GTK_CONTAINER(widget), SetFontRecursively2, font_desc);
+   }
+}
+# else
 void SetFontRecursively(GtkWidget *widget, gpointer data)
 {
    GtkStyle *style;
@@ -1522,11 +1542,68 @@ void SetFontRecursively(GtkWidget *widget, gpointer data)
    f = (GdkFont *)data;
 
    style = gtk_widget_get_style(widget);
-   style->font=f;
    gtk_widget_set_style(widget, style);
    if (GTK_IS_CONTAINER(widget)) {
       gtk_container_foreach(GTK_CONTAINER(widget), SetFontRecursively, f);
    }
+}
+# endif
+void font_selection_ok(GtkWidget *w, GtkFontSelectionDialog *fs)
+{
+   gchar *s = gtk_font_selection_dialog_get_font_name(fs);
+
+   g_print("%s\n", s);//removeme
+# ifndef ENABLE_GTK2
+   GdkFont *f;
+# endif
+
+# ifdef ENABLE_GTK2
+   SetFontRecursively2(window, s);
+# else
+   f=gdk_fontset_load(s);
+   SetFontRecursively(window, f);
+# endif
+
+   g_free(s);
+   gtk_widget_destroy(GTK_WIDGET(fs));
+   cb_app_button(NULL, GINT_TO_POINTER(REDRAW));
+}
+
+void font_sel_dialog()
+{
+   static GtkWidget *fontsel = NULL;
+
+   if (!fontsel) {
+      fontsel = gtk_font_selection_dialog_new("Font Selection Dialog");
+      gtk_window_set_position(GTK_WINDOW(fontsel), GTK_WIN_POS_MOUSE);
+
+      gtk_signal_connect(GTK_OBJECT(fontsel), "destroy",
+			 GTK_SIGNAL_FUNC(gtk_widget_destroyed),
+			 &fontsel);
+
+      gtk_window_set_modal(GTK_WINDOW(fontsel), TRUE);
+
+      gtk_signal_connect(GTK_OBJECT(GTK_FONT_SELECTION_DIALOG(fontsel)->ok_button),
+			 "clicked", GTK_SIGNAL_FUNC(font_selection_ok),
+			 GTK_FONT_SELECTION_DIALOG(fontsel));
+      gtk_signal_connect_object(GTK_OBJECT(GTK_FONT_SELECTION_DIALOG(fontsel)->cancel_button),
+				"clicked", GTK_SIGNAL_FUNC(gtk_widget_destroy),
+				GTK_OBJECT(fontsel));
+     }
+
+   if (!GTK_WIDGET_VISIBLE(fontsel)) {
+      gtk_widget_show(fontsel);
+   } else {
+      gtk_widget_destroy(fontsel);
+   }
+}
+
+/*fixme move to font.c */
+void cb_font(GtkWidget *widget, gpointer data)
+{
+   font_sel_dialog();
+
+   return;
 }
 #endif
 
@@ -1565,7 +1642,9 @@ int main(int argc,
    jp_startup_info info;
 #endif
 #ifdef FONT_TEST
+# ifndef ENABLE_GTK2
    GdkFont *f;
+# endif
 #endif   
 char *xpm_locked[] = {
    "15 18 4 1",
@@ -1769,6 +1848,9 @@ char *xpm_unlocked[] = {
    setlocale(LC_ALL, "");
 #endif
    bindtextdomain(EPN, LOCALEDIR);
+#ifdef ENABLE_GTK2
+   bind_textdomain_codeset(EPN, "UTF-8");
+#endif
    textdomain(EPN);
 #endif
 
@@ -1846,6 +1928,15 @@ char *xpm_unlocked[] = {
 
    gtk_signal_connect(GTK_OBJECT(window), "focus_out_event",
 		      GTK_SIGNAL_FUNC(cb_focus), GINT_TO_POINTER(0));
+#endif
+#if 0 /* removeme */
+     {
+	GtkStyle *style;
+	style = gtk_widget_get_style(window);
+	pango_font_description_free(style->font_desc);
+	style->font_desc = pango_font_description_from_string("Sans 16");
+	gtk_widget_set_style(window, style);
+     }
 #endif
 
    /* Set a handler for delete_event that immediately */
@@ -2019,6 +2110,14 @@ char *xpm_unlocked[] = {
    gtk_tooltips_set_tip(glob_tooltips, button, _("Sync your palm to the desktop"), NULL);
 
 
+#ifdef FONT_TEST
+   /* Create "Font" button */
+   button = gtk_button_new_with_label(_("Font"));
+   gtk_signal_connect(GTK_OBJECT(button), "clicked",
+		      GTK_SIGNAL_FUNC(cb_font), NULL);
+
+   gtk_box_pack_start(GTK_BOX(g_vbox0), button, FALSE, FALSE, 0);
+#endif
    /* Create "Backup" button in left column */
    button = gtk_button_new_with_label(_("Backup"));
    gtk_signal_connect(GTK_OBJECT(button), "clicked",
@@ -2155,8 +2254,11 @@ char *xpm_unlocked[] = {
    jp_logf(JP_LOG_DEBUG, "w=%d, h=%d\n", w, h);
 
 #ifdef FONT_TEST
-   f=gdk_fontset_load("-adobe-utopia-medium-r-normal-*-*-200-*-*-p-*-iso8859-1");
-   SetFontRecursively(window, f);
+//   f=gdk_fontset_load("-adobe-utopia-medium-r-normal-*-*-200-*-*-p-*-iso8859-1");
+//   f=gdk_fontset_load("-adobe-utopia-bold-i-normal-*-*-100-*-*-p-*-iso8859-2");
+//   SetFontRecursively(window, f);
+//   SetFontRecursively2(window, "Sans 14");
+//   SetFontRecursively2(menubar, "Sans 16");
 #endif
 #ifdef ALARMS
    alarms_init(skip_past_alarms, skip_all_alarms);
