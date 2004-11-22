90901f0523aced6398ebac46ad57ca3dc689deb6
Rik Wehbring <rikw00@yahoo.com>
2004-11-22 02:40:43 +0000

    Unused UTF conversion functions deleted

--- cp1250.h
+++ cp1250.h
@@ -3,22 +3,8 @@
  * Convert Palm  <-> Unix:
  * Palm : CP 1250
  * Unix : ISO-8859-2
- *   and
- * Palm : CP 1250
- * Unix : UTF-8
- *   and also (JPA)
- * Palm : CP 1252
- * Unix : UTF-8
  */
 
 void Win2Lat(char *const buf, int buf_len);
 void Lat2Win(char *const buf, int buf_len);
-
-int Win2UTFsz(const unsigned char *buf, int buf_len); /* JPA */
-/* void Win2UTF(unsigned char *const buf, int buf_len); JPA */
-unsigned char *Win2UTF(const unsigned char *buf, int buf_len); /* JPA */
-void UTF2Win(unsigned char *const buf, int buf_len);
-
-unsigned char *Lat2UTF(const unsigned char *buf, int buf_len);
-void UTF2Lat(char *const buf, int buf_len);
 
