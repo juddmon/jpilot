e27bc6e498210e279aaf9113ca9d5ed35219e549
Rik Wehbring <rikw00@yahoo.com>
2004-11-29 06:24:46 +0000

    Add GNU public license to untagged files

--- japanese.c
+++ japanese.c
@@ -1,3 +1,25 @@
+/* $Id: japanese.c,v 1.9 2004/11/29 06:24:46 rikster5 Exp $ */
+
+/*******************************************************************************
+ * japanese.c
+ * A module of J-Pilot http://jpilot.org
+ *
+ * Copyright (C) 1999 by Hiroshi Kawashima
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ******************************************************************************/
+
 /*
 	Japanization library
 	Convert Palm <-> Unix Japanese Code, ie:
--- russian.c
+++ russian.c
@@ -1,3 +1,25 @@
+/* $Id: russian.c,v 1.5 2004/11/29 06:24:46 rikster5 Exp $ */
+
+/*******************************************************************************
+ * russian.c
+ * A module of J-Pilot http://jpilot.org
+ *
+ * Copyright (C) 2000 by Gennady Kudelya
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ******************************************************************************/
+
 /*
 	Convert Palm  <-> Unix:
 		Palm : koi8
