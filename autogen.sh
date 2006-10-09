216f610329d9873ae4bad7a02881198cd154500d
Rik Wehbring <rikw00@yahoo.com>
2006-10-09 23:57:50 +0000

    Overhaul autotools environment
    Added lots of explicit comments about how scripts are operating
    Removed accumulated cruft
    Fixed Bug 1721
    Fixed configure script where --enable-option could interfere with building
    correctly if the default for the option was already to be enabled.
    Fixed many potential problems in the configure sh script where unquoted
    variables were being compared to unquoted strings.

--- autogen.sh
+++ autogen.sh
@@ -1,19 +1,23 @@
-# $Id: autogen.sh,v 1.13 2006/09/28 04:12:21 rikster5 Exp $
+# $Id: autogen.sh,v 1.14 2006/10/09 23:57:49 rikster5 Exp $
 
-# Echo commands before they execute to help show 
+# Echo commands before they execute to help show progress
 set -x
 
-# Clear the files that will be rebuilt by autoconf, automake, & autoheader
+# Clear files that will be rebuilt by autoconf, automake, & autoheader
 rm -f configure Makefile Makefile.in config.h.in
 
 # Create aclocal.m4, macros that are used in all subsequent processing
 aclocal -I m4
 
+# Prepare build for gettext.  Must precede intltoolize
+gettextize --force --copy --no-changelog
+# Copy over scripts to allow internationalization
+intltoolize --force --copy --automake
 # Copy over scripts for libtool
 libtoolize --force --copy --automake
 
-# Copy over scripts to allow localization and internationalization
-intltoolize --force --copy --automake
+# Update aclocal after other scripts have run
+aclocal -I m4
 
 # Create config.h.in from configure.in
 autoheader
