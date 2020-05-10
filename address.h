2e4c86fff0a9211951e04ab3147f12ef2b8cedc8
dmalia1 <althor@ksu.edu>
2020-05-10 16:27:44 -0500

    got address to load..

--- address.h
+++ address.h
@@ -34,8 +34,8 @@ typedef struct {
 } address_schema_entry;
 
 static char *const ADDRESS_LAST_NAME_COMPANY = "Last Name/Company";
-static char *const FIRST_NAME_COMPANY = "First Name/Company";
-static char *const COMPANY_LAST_NAME = "Company/Last Name";
+static char *const ADDRESS_FIRST_NAME_COMPANY = "First Name/Company";
+static char *const ADDRESS_COMPANY_LAST_NAME = "Company/Last Name";
 #define NUM_IMS 2
 #define NUM_ADDRESSES 3
 #define NUM_CONTACT_NOTEBOOK_PAGES 6
